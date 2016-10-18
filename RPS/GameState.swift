3bd634f6c945efa7d89f4044e21bd34b14e5d801
Xiang, Zhuyuan <zhuyuan.xiang@rakuten.com>
2016-10-18 12:05:44 +0900

    import

--- RPS/GameState.swift
+++ RPS/GameState.swift
@@ -13,3 +13,60 @@ enum GameState : String{
     case Draw
     case Start
 }
+
+
+func cacl(_ sys_res:Sign, _ user_res:String) -> String {
+    var result = ""
+    switch sys_res {
+    case .paper:
+        switch user_res{
+        case "✌️":
+            
+            result =   GameState.Win.rawValue
+        case "👊":
+            result = GameState.Lost.rawValue
+        case "✋":
+            result = GameState.Draw.rawValue
+        default:
+            result = ""
+        }
+        
+        
+    case .rock:
+        
+        
+        switch user_res{
+        case "✌️":
+            result =   GameState.Lost.rawValue
+            
+            
+        case "👊":
+            result =   GameState.Draw.rawValue
+            
+        case "✋":
+            
+            result =  GameState.Win.rawValue
+        default:
+            result = ""
+        }
+        
+    case .scissors:
+        
+        
+        switch user_res{
+        case "✌️":
+            result = GameState.Draw.rawValue
+            
+        case "👊":
+            result = GameState.Win.rawValue
+            
+        case "✋":
+            result = GameState.Lost.rawValue
+        default:
+            result = ""
+            
+        }
+    }
+    return result
+    
+}
--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBOutlet weak var message: UILabel!
     @IBOutlet weak var logo: UILabel!
     
@@ -30,124 +30,58 @@ class ViewController: UIViewController {
     
     
     @IBAction func Play(_ sender: UIButton) {
+        
         hand.isHidden = false
         fisted.isHidden = false
         victory.isHidden = false
         play.isHidden = true
+        UIView.performWithoutAnimation {
+     
+            hand.setTitle("✋", for: UIControlState.normal)
+            fisted.setTitle("👊", for: UIControlState.normal)
+            victory.setTitle("✌️", for: UIControlState.normal)
+            hand.isEnabled = true
+            logo.text! = "🤖"
+            message.text! = "石头,剪刀,布!"
+        }
         
-        hand.setTitle("✋", for: UIControlState.normal)
-        fisted.setTitle("👊", for: UIControlState.normal)
-        victory.setTitle("✌️", for: UIControlState.normal)
-        hand.isEnabled = true
-        logo.text! = "🤖"
-        message.text! = "石头,剪刀,布!"
-
     }
-
-    @IBAction func Show(_ sender: UIButton) {
     
-//        switch sender {
-//        case victory:
-//            hand.isHidden = true
-//            fisted.isHidden = true
-//        case hand:
-//            fisted.isHidden = true
-//            victory.isHidden = true
-//        case fisted:
-//            hand.isHidden = true
-//            victory.isHidden = true
-//        default:
-//            hand.isHidden = false
-//            fisted.isHidden = false
-//            victory.isHidden = false
-//            
-//        }
-        
+    @IBAction func Show(_ sender: UIButton) {
         // hidden left and right
         fisted.isHidden = true
         victory.isHidden = true
-        hand.setTitle(sender.currentTitle!, for: .normal)
+        play.isHidden = false
         hand.isEnabled = false
-        
+        UIView.performWithoutAnimation {
+            hand.setTitle(sender.currentTitle!, for: .normal)
+        }
         // get system show
         let sys_res = randomSign()
         let user_res = sender.currentTitle!
-        var result = ""
-        switch sys_res {
-        case .paper:
-            switch user_res{
-                case "✌️":
-                
-                  result =   GameState.Win.rawValue
-                case "👊":
-                    result = GameState.Lost.rawValue
-                case "✋":
-                    result = GameState.Draw.rawValue
-            default:
-                result = ""
-            }
-            
+        let result = cacl(sys_res, user_res)
         
-        case .rock:
-            
-            
-            switch user_res{
-            case "✌️":
-                result =   GameState.Lost.rawValue
-
-                
-            case "👊":
-                result =   GameState.Draw.rawValue
-                
-            case "✋":
-                
-               result =  GameState.Win.rawValue
-            default:
-                result = ""
-            }
-
-        case .scissors:
-            
-            
-            switch user_res{
-            case "✌️":
-                result = GameState.Draw.rawValue
-                
-            case "👊":
-                result = GameState.Win.rawValue
-                
-            case "✋":
-                result = GameState.Lost.rawValue
-            default:
-                result = ""
-                
-            }
-        }
-//        // create the alert
-//        let alert = UIAlertController(title: "My Title", message: result, preferredStyle: UIAlertControllerStyle.alert)
-//        
-//        // add an action (button)
-//        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-//        
-//        // show the alert
-//        self.present(alert, animated: true, completion: nil)
+        //        // create the alert
+        //        let alert = UIAlertController(title: "My Title", message: result, preferredStyle: UIAlertControllerStyle.alert)
+        //
+        //        // add an action (button)
+        //        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        //
+        //        // show the alert
+        //        self.present(alert, animated: true, completion: nil)
         
         switch sys_res{
         case .rock:
-           logo.text! =  "👊"
+            logo.text! =  "👊"
         case .paper:
-           logo.text! =  "✋"
+            logo.text! =  "✋"
         case .scissors:
-           logo.text! =  "✌️"
+            logo.text! =  "✌️"
         default:
-           logo.text! =  ""
-        
+            logo.text! =  ""
+            
         }
         message.text! = " You " + result
-        play.isHidden = false
-        // hide the others
-        
-        
         
     }
 }
